5bab8a48e0cc37904e624606e73f87741773db6c
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-04-25 11:43:33 +0200

    Dlt structure

---
+++ main.py
@@ -0,0 +1,5 @@
+import dlt.pipeline
+
+if __name__ == "__main__":
+    # pipeline.run()  # Uncomment this line to run the pipeline
+    print("Pipeline is ready to run. Uncomment the pipeline.run() line to execute it.")
