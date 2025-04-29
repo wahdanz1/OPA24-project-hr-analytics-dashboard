12312c774d9b7a49592d3e7199317b45f4a10fbd
Oskara1209 <oskar.akerman12@outlook.com>
2025-04-29 12:25:43 +0200

    created src models

---
+++ hr_project/models/src/src_occupation.sql
@@ -0,0 +1,10 @@
+WITH stg_job_ads AS (SELECT * FROM {{ source('hr_job_ads', 'stg_ads') }})
+
+select 
+  occupation__concept_id AS occupation_id,
+  occupation__label AS occupation,
+  occupation_group__concept_id AS occupation_group_id, 
+  occupation_group__label AS occupation_group,
+  occupation_field__concept_id AS occupation_field_id,
+  occupation_field__label AS occupation_field_label
+FROM stg_job_ads
