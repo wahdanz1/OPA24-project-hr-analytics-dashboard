048fb2d6dfd548a339e396b9902312b6f0902bfa
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-06 11:11:17 +0200

    Refactored method for fetching data from db for modular use

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -2,25 +2,6 @@ import pandas as pd
 from pipeline.db import DuckDBConnection
 from config import db_path
 
-def get_distinct_occupations() -> pd.DataFrame:
-    """Fetch distinct occupations per municipality from the database."""    
-    query = f"""
-        SELECT DISTINCT
-        COUNT(o.occupation_id) AS distinct_occupations,
-        e.workplace_municipality
-        FROM refined.fct_job_ads ja
-        
-        JOIN refined.dim_employer e
-            ON e.employer_id = ja.employer_id
-        JOIN refined.dim_occupation o
-            ON o.occupation_id = ja.occupation_id
-        
-        WHERE workplace_municipality IS NOT NULL
-
-        GROUP BY e.workplace_municipality
-        ORDER BY distinct_occupations DESC
-        LIMIT 15
-    """
-
+def fetch_data_from_db(query: str) -> pd.DataFrame:
     with DuckDBConnection(db_path) as conn:
         return conn.query(query)
