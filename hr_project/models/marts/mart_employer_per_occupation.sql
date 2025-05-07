2133376358b6b5e2ac32876cdf4d7df7fd3c92f1
Oskara1209 <oskar.akerman12@outlook.com>
2025-05-07 17:31:15 +0200

    Created int and mart for employer per occupation

---
+++ hr_project/models/marts/mart_employer_per_occupation.sql
@@ -0,0 +1,9 @@
+WITH int_ads_per_employer_occupation AS (SELECT * FROM {{ ref('int_ads_per_employer_occupation') }})
+SELECT 
+    occupation,
+    employer_name,
+    ads_posted_last_30_days,
+    RANK() OVER (PARTITION BY occupation ORDER BY ads_posted_last_30_days DESC) AS employer_rank
+FROM int_ads_per_employer_occupation
+QUALIFY employer_rank <= 10;
+
