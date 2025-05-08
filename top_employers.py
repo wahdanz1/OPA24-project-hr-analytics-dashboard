854cc98eb3c8e51a3057e63740f7b65a90bccf9f
Oskara1209 <oskar.akerman12@outlook.com>
2025-05-08 15:15:18 +0200

    minor update

--- top_employers.py
+++ top_employers.py
@@ -2,5 +2,5 @@ import streamlit as st
 from dashboard.utils import fetch_data_from_db
 from dashboard.plots import create_horizontal_bar_chart
 
-def top_employers_per_occupation():
-    st.write()
+def top_employers_page():
+    st.header("Top Employers per Occupation", divider=True)
