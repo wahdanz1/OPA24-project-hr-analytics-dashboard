3607e0200108aef131cf2a72041167d9092b5be7
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-05-08 15:24:35 +0200

    Updated width of sidebar

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ st.set_page_config(layout="wide")
 
 st.title("HR Dashboard")
 
+st.markdown(
+    """
+    <style>
+        section[data-testid="stSidebar"] {
+            width: 300px !important; # Set the width to your desired value
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 with st.sidebar:
     page_selection = st.radio(
         "Choose a statistics page",
