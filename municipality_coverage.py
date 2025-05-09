74622adb5528eb9e81a927a36540d853c54c8805
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-09 16:17:29 +0200

    Remove top 3 occupations per city function

--- municipality_coverage.py
+++ municipality_coverage.py
@@ -1,19 +1,17 @@
 import streamlit as st
 from dashboard.utils import fetch_data_from_db, get_occupation_field_name_string
-from dashboard.plots import create_horizontal_bar_chart, create_vertical_bar_chart
+from dashboard.plots import create_horizontal_bar_chart
 
 def municipality_coverage_page():
     st.header("Municipality Coverage", divider=True)
     st.markdown("This page provides insights into the coverage of job ads across different municipalities.")
     distinct_occupations_per_municipality()
-    top_3_occupations_per_city()
 
 def distinct_occupations_per_municipality():
     # Description for the first graph
     st.markdown("This graph shows the number of distinct occupations per municipality.")
 
-    # Build the WHERE clause for the SQL query based on the selected occupation field
-    
+    # Build the string for the SQL query based on the selected occupation field
     name_string = get_occupation_field_name_string()
 
     # Send a query to the database to get the data for the graph
@@ -47,47 +45,3 @@ def distinct_occupations_per_municipality():
         st.plotly_chart(fig1, use_container_width=True)
 
     st.divider()
-
-def top_3_occupations_per_city():
-    # Description for the first graph
-    st.markdown("This graph shows the top 3 occupations per city. You can filter the data by municipality.")
-
-    # Build the WHERE clause for the SQL query based on the selected occupation field
-    name_string = get_occupation_field_name_string()
-
-    # Send a query to the database to get the data for the graph
-    query2 = f"""
-            SELECT *
-            FROM marts.mart_top_occupations_per_city
-            WHERE occupation_field IN ({name_string})
-        """
-    data2 = fetch_data_from_db(query2)
-
-    unique_municipalities = sorted(data2["workplace_municipality"].dropna().unique())
-
-    selected_municipalities = st.multiselect(
-        "Select one or more municipalities",
-        options=unique_municipalities,
-        default='Göteborg',  # Default to the first municipality
-    )
-
-    filtered_data2 = data2[data2["workplace_municipality"].isin(selected_municipalities)]
-
-    # Check if the data is empty before plotting
-    # If the data is not empty, create a horizontal bar chart using Plotly
-    if not filtered_data2.empty:
-        fig2 = create_vertical_bar_chart(
-            filtered_data2,
-            x_value="workplace_city",
-            y_value="job_ad_count",
-            x_label="City",
-            y_label="Number of job ads",
-            title="Top 3 Occupations per City",
-            color_column="occupation",
-            )
-        st.plotly_chart(fig2, use_container_width=True)
-    else:
-        st.warning("No data available for the selected municipality.")
-
-    st.divider()
-
