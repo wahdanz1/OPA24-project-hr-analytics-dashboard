e24c4e7c790bca277e28b225f244bfc9eec4861d
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-05-12 16:36:40 +0200

    Added a method that cleans the occupation field names for display

--- summary.py
+++ summary.py
@@ -4,18 +4,19 @@ from dashboard.utils import fetch_data_from_db, get_sidebar_filters
 
 def summary_page():
     st.header("Summary", divider=True)
-    everything_method()
 
-    pass
-
-def everything_method():
-
+    occupation_fields = get_selected_occupation_fields()
+    st.markdown(f"**Selected Occupation Fields:**  {occupation_fields}")
     display_kpis()
 
-    pass
+
+
+
+    
+
 
 def display_kpis():
-    # Fetch data from the database
+    
     selected_posted = get_jobs_posted_selected_period()
     st.metric(label="Jobs Posted in Selected Period", value=selected_posted, delta=None)
 
@@ -40,3 +41,16 @@ def get_jobs_posted_selected_period():
     df = fetch_data_from_db(query)
     return df['jobs_posted'].values[0]
 
+
+# Returns the selected occupation fields from the sidebar
+# and formats them for display
+def get_selected_occupation_fields():
+    # Get the sidebar filters
+    name_string, limit_value, start_day, end_day = get_sidebar_filters()
+    occupation_list = name_string.split(",")
+    # Remove Fnutts from the list
+    formatted_list = [x.strip("'") for x in occupation_list]
+    # Format the list with bold text
+    formatted_string = ", ".join([f"{x}" for x in formatted_list])
+    # Return the formatted string
+    return formatted_string
