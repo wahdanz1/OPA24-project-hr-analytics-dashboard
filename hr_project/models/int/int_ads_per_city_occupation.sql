79ff83f7c83836fe9dbf5aa431e2f5f0c8d0f3fe
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-12 16:42:00 +0200

    Minor formatting

--- hr_project/models/int/int_ads_per_city_occupation.sql
+++ hr_project/models/int/int_ads_per_city_occupation.sql
@@ -1,6 +1,9 @@
 -- This intermediate model is used to analyze the number of job ads per city and occupation.
 -- It is used to analyze the most common (top 3) occupations in each city.
-WITH fct_job_ads AS (SELECT * FROM {{ ref('fct_job_ads') }})
+WITH fct_job_ads AS (
+    SELECT * FROM {{ ref('fct_job_ads') }}
+)
+
 SELECT
     ja.publication_date,
     e.workplace_municipality,
@@ -8,11 +11,12 @@ SELECT
     o.occupation,
     o.occupation_field,
     COUNT(DISTINCT ja.job_details_id) AS job_ad_count
-    FROM fct_job_ads ja
-    
-    JOIN refined.dim_employer e
-        ON e.employer_id = ja.employer_id
-    JOIN refined.dim_occupation o
-        ON o.occupation_id = ja.occupation_id
+FROM fct_job_ads ja
 
-    GROUP BY e.workplace_municipality, e.workplace_city, o.occupation, o.occupation_field, ja.publication_date
+JOIN refined.dim_employer e
+    ON e.employer_id = ja.employer_id
+
+JOIN refined.dim_occupation o
+    ON o.occupation_id = ja.occupation_id
+
+GROUP BY e.workplace_municipality, e.workplace_city, o.occupation, o.occupation_field, ja.publication_date
