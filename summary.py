5484dcec4140ee7530bea1dce7ee46fab6fc9ac3
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-05-12 15:54:12 +0200

    Created a summary file meant as a main screen with kpis and important stuff

---
+++ summary.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from dashboard.utils import fetch_data_from_db, get_sidebar_filters
+
+def summary_page():
+    st.header("Summary", divider=True)
+    pass
