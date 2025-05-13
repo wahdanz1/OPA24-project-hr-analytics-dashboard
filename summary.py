0cecc5c4e3a0ed6a393466d5aab4865b327ccaf0
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-05-13 10:15:43 +0200

    Added top 5 occupations to summary

--- summary.py
+++ summary.py
@@ -19,11 +19,16 @@ def display_kpis():
     
     selected_posted = get_jobs_posted_selected_period()
     st.metric(label="Jobs Posted in Selected Period", value=selected_posted, delta=None)
+    top_occupations = get_top_occupations()
+    st.subheader("Top 5 Occupations")
+    st.dataframe(top_occupations, use_container_width=True)
+    # Display the top 5 occupations
 
 
 
-
-
+################################################
+#   Methods that return values for the KPIs    #
+################################################
 
 # Returns the number of jobs posted in the selected period
 def get_jobs_posted_selected_period():
@@ -40,6 +45,31 @@ def get_jobs_posted_selected_period():
     """
     df = fetch_data_from_db(query)
     return df['jobs_posted'].values[0]
+
+
+def get_top_occupations():
+    # Get the sidebar filters
+    name_string, limit_value, start_day, end_day = get_sidebar_filters()
+
+    # Query to get the top occupations
+    query = f"""
+        SELECT d.occupation, COUNT(*) AS ad_count
+        FROM refined.fct_job_ads f
+        JOIN refined.dim_occupation d ON f.occupation_id = d.occupation_id
+
+        WHERE publication_date BETWEEN (NOW() - INTERVAL {end_day} DAY)
+        AND (NOW() - INTERVAL {start_day} DAY)
+        AND occupation_field IN ({name_string})
+        GROUP BY d.occupation
+        ORDER BY ad_count DESC
+        LIMIT 5;
+
+    """
+    df = fetch_data_from_db(query)
+    return df
+
+
+
 
 
 # Returns the selected occupation fields from the sidebar
