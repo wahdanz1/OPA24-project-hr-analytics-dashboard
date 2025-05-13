246b55532d39a6f250f03a883cca7ec2bc3b8aba
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-13 15:59:59 +0200

    Remove distinct occupations per municipality function/plot entirely, and added top 3 occupations per municipality function/plot

--- municipality_coverage.py
+++ municipality_coverage.py
@@ -1,50 +1,95 @@
 import streamlit as st
+import pandas as pd
 from dashboard.utils import fetch_data_from_db, get_sidebar_filters
-from dashboard.plots import create_horizontal_bar_chart
+from dashboard.plots import create_horizontal_bar_chart, create_vertical_bar_chart
 
 def municipality_coverage_page():
     st.header("Municipality Coverage", divider=True)
     st.markdown("This page provides insights into the coverage of job ads across different municipalities.")
-    distinct_occupations_per_municipality()
+    top_occupations_per_municipality()
+    # st.divider()
 
-def distinct_occupations_per_municipality():
-    # Description for the first graph
-    st.markdown("This graph shows the number of distinct occupations per municipality.")
+def top_occupations_per_municipality():
+    # Description for the graph
+    st.markdown("This graph shows the top occupations per municipality based on total vacancies.")
 
     # Build the string for the SQL query based on the selected occupation field
     name_string, limit_value, start_day, end_day = get_sidebar_filters()
 
+    region_query = """
+                    SELECT DISTINCT workplace_region
+                    FROM marts.mart_top_occupations_per_municipality
+                    WHERE workplace_region IS NOT NULL
+                    ORDER BY workplace_region
+                    """
+    regions = fetch_data_from_db(region_query)
+    region_options = regions["workplace_region"].tolist()
+    
+    # Region selection for the graph
+    selected_region = st.selectbox(
+        "Select a region",
+        options=region_options,
+        index=0,
+        key="region_selectbox"
+    )
+
     # Send a query to the database to get the data for the graph
     query1 = f"""
+        WITH filtered_data AS (
             SELECT
                 workplace_municipality,
-                COUNT(DISTINCT occupation_id) AS distinct_occupations
-            FROM marts.mart_distinct_occupations_per_municipality
-            WHERE occupation_field IN ({name_string})
-            AND publication_date
-                BETWEEN (NOW() - INTERVAL {end_day} DAY)
-                    AND (NOW() - INTERVAL {start_day} DAY)
-            GROUP BY workplace_municipality
-            ORDER BY distinct_occupations DESC
-            LIMIT {limit_value}
-            """
-    st.code(query1, language="sql")
+                occupation,
+                occupation_field,
+                workplace_region,
+                SUM(total_vacancies) AS total_vacancies
+            FROM marts.mart_top_occupations_dynamic
+            WHERE occupation_field IN ('Hälso- och sjukvård')
+            AND workplace_region = '{selected_region}'
+            AND publication_date BETWEEN (CURRENT_DATE - INTERVAL '{end_day}' DAY)
+                                    AND (CURRENT_DATE - INTERVAL '{start_day}' DAY)
+            GROUP BY workplace_municipality, occupation, occupation_field, workplace_region
+        )
+
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY workplace_municipality, occupation_field
+                ORDER BY total_vacancies DESC
+            ) AS rank
+        FROM filtered_data
+        WHERE total_vacancies IS NOT NULL
+        QUALIFY rank <= 3
+        ORDER BY total_vacancies ASC
+    """
     data1 = fetch_data_from_db(query1)
 
     # Check if the data is empty before plotting
-    # If the data is not empty, create a horizontal bar chart using Plotly
+    # If the data is not empty, create a vertical bar chart using Plotly
     if not data1.empty:
-        # Sort by distinct_occupations in descending order
-        data1 = data1.sort_values(by="distinct_occupations", ascending=True)
+        # Add if-statement for whether data1 contains a lot of rows, meaning the
+        # selected region has a lot of municipalities which would cause the chart
+        # to be too wide
 
-        fig1 = create_horizontal_bar_chart(
+        # Sort by municipality and rank to ensure the order of the bars in the bar chart
+        data1 = data1.sort_values(by=["workplace_municipality", "rank"])
+
+        # Reverse order for the rank
+        data1["rank"] = 4 - data1["rank"]  # If rank is 1, score is 3; rank 2 → 2; rank 3 → 1
+
+        fig1 = create_vertical_bar_chart(
             data1,
-            x_value="distinct_occupations",
-            y_value="workplace_municipality",
-            title="Distinct Occupations per Municipality",
-            color_column="distinct_occupations",
-            hover_template="%{y} has %{x} distinct occupations",
+            x_value="workplace_municipality",
+            x_label="Municipality",
+            y_value="rank",
+            y_label="Vacancies per occupation",
+            title=f"Top Occupations per Municipality (in {selected_region})",
+            color_column="occupation",
+            hover_data={"total_vacancies": True,
+                        "rank": False,
+                        "workplace_municipality": False},
             )
         st.plotly_chart(fig1, use_container_width=True)
 
-    st.divider()
+        with st.expander("SQL Code", expanded=False):
+            st.markdown("This is the SQL code used to generate the graph:")
+            st.code(query1, language="sql")
