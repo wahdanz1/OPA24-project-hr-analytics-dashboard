c78e57617fcad5c8689aff8d5e9ea2c606f8fdc5
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-26 13:22:05 +0200

    Updated test to use DATE correctly

--- job_market/tests/test_future_publication_date.sql
+++ job_market/tests/test_future_publication_date.sql
@@ -1,7 +1,7 @@
 -- Checks for any job ads with a publication date set in the future,
 -- which could indicate incorrect or dirty data.
 
-SELECT 
-    * 
-FROM {{ ref('src_job_ads') }} 
-WHERE publication_date > CURRENT_DATE
+SELECT
+    *
+FROM {{ ref('src_job_ads') }}
+WHERE CAST(publication_date AS DATE) > CURRENT_DATE
