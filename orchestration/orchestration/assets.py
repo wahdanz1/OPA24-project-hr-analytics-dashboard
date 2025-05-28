125b900c9b23575832699f83c04f3a3e4f404f0d
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-28 12:53:56 +0200

    Created dlt asset

--- orchestration/orchestration/assets.py
+++ orchestration/orchestration/assets.py
@@ -1,1 +1,45 @@
+########################
+#        Setup         #
+########################
 
+# Import necessary Dagster packages
+import dagster as dg
+from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
+from dagster_dlt import DagsterDltResource, dlt_assets
+
+import dlt
+
+# To be able to import config.py and access its variables
+import sys
+from pathlib import Path
+
+# Add the parent directory (root, 2 levels up) to sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+
+
+################################
+#        Param-builder         #
+################################
+
+# Import necessary functions/variables
+from pipeline.resources import jobsearch_source
+from config import db_path
+from pipeline.utils import make_params_list
+
+
+############################
+#        dlt asset         #
+############################
+
+dlt_resource = DagsterDltResource()
+
+@dlt_assets(
+    dlt_source=jobsearch_source(make_params_list(day_range=3)),
+    dlt_pipeline=dlt.pipeline(
+        pipeline_name="job_ads_pipeline",
+        dataset_name="staging",
+        destination=dlt.destinations.duckdb(str(db_path)),
+    ),
+)
+def job_ads_dlt_asset(context: dg.AssetExecutionContext, dlt: DagsterDltResource):
+    yield from dlt.run(context=context)
--- orchestration/orchestration/definitions.py
+++ orchestration/orchestration/definitions.py
@@ -1,9 +1,11 @@
 from dagster import Definitions, load_assets_from_modules
-
-from orchestration import assets  # noqa: TID252
+from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+from .assets import dlt_resource
+
 defs = Definitions(
     assets=all_assets,
+    resources={"dlt": dlt_resource},
 )
