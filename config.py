0a66817eeea9c0b1e1b0673317d0089313566d11
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-05-28 17:05:26 +0200

    Minor update to config.py

--- config.py
+++ config.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 
+# Base URL for the JobTech API
 base_url = "https://jobsearch.api.jobtechdev.se"
 
+# Endpoint parameters
 default_limit = 100
 default_offset = 0
-table_name = "job_ads"
 
+# Occupation field dictionary mapping
 occupation_field_dict = {
     "Administration, ekonomi, juridik"   : "X82t_awd_Qyc",  
     "Försäljning, inköp, marknadsföring" : "RPTn_bxG_ExZ",
     "Hälso- och sjukvård"                : "NYW6_mP6_vwf",  
 }
 
-
-
+# Directory of the config.py file
 working_directory = Path(__file__).resolve().parent
+
+# Database configuration
+table_name = "job_ads"
 
 dbt_folder = "job_market" # Database file directory
 dbt_path = working_directory / dbt_folder  # Path to the database folder
@@ -22,4 +26,3 @@ db_filename = "job_ads.duckdb" # Database file name
 db_path = working_directory / dbt_folder / db_filename  # Full path to the database file
 
 profiles_dir = Path.home() / ".dbt"
-  
